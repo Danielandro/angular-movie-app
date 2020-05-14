dc1d03b70ea40f37bbf3b09d10dd03b3f3664411
Danny <mtme13@hotmail.com>
2020-05-14 14:50:14 +0100

    Finally centered cover image in image container * removed card component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './components/slider/slider.component';
-import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [SliderComponent, CardComponent],
+  declarations: [SliderComponent],
   imports: [
     CommonModule
   ],
-  exports: [SliderComponent, CardComponent]
+  exports: [SliderComponent]
 })
 export class SharedModule { }
